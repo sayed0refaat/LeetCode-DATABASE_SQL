d4dd925952e9a931df1ece3762b146d5ba551bce
Sayed Refaat <61937123+sayed0refaat@users.noreply.github.com>
2024-02-08 05:29:13 +0200

    Add files via upload

---
+++ 1070. Product Sales Analysis III.sql
@@ -0,0 +1,18 @@
+WITH FirstYearSales AS (
+    SELECT
+        s.product_id,
+        MIN(s.year) AS first_year
+    FROM
+        Sales s
+    GROUP BY
+        s.product_id
+)
+
+SELECT
+    s.product_id,
+    s.year as first_year,
+    s.quantity,
+    s.price
+FROM
+    Sales s
+    JOIN FirstYearSales fys ON s.product_id = fys.product_id AND s.year = fys.first_year;
---
+++ 602. Friend Requests II Who Has the Most Friends.sql
@@ -0,0 +1,7 @@
+
+with base as(select requester_id id from RequestAccepted
+union all
+select accepter_id id from RequestAccepted)
+
+
+select id, count(*) num  from base group by id order by num desc limit 1
