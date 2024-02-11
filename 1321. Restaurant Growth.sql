0b7e8236a4c907585b0adf100e8a53953ef4a0e8
Sayed Refaat <61937123+sayed0refaat@users.noreply.github.com>
2024-02-11 04:03:45 +0200

    Add files via upload

---
+++ 1321. Restaurant Growth.sql
@@ -0,0 +1,22 @@
+# i stucked , so i searched the solutions 
+SELECT
+    visited_on,
+    (
+        SELECT SUM(amount)
+        FROM customer
+        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+    ) AS amount,
+    ROUND(
+        (
+            SELECT SUM(amount) / 7
+            FROM customer
+            WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+        ),
+        2
+    ) AS average_amount
+FROM customer c
+WHERE visited_on >= (
+        SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
+        FROM customer
+    )
+GROUP BY visited_on;
